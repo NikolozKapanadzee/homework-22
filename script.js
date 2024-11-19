5c0902306f4f41d011e6e112f6137256c6439a9c
NikolozKapanadzee <128920612+NikolozKapanadzee@users.noreply.github.com>
2024-11-20 02:30:10 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// //exe 1
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(num => {
+//     console.log(num * num);
+// });
+
+// // exe 2
+
+// const fruits = ['vashli', 'banani', 'komshi', 'sazamtro'];
+// fruits.forEach(fruit => {
+//     console.log(fruit);
+// });
+
+// //exe 3
+// const nums = [10, 20, 30, 40];
+// let sum = 0;
+// nums.forEach(num => {
+//     sum += num;
+// });
+// console.log(sum);
+
+
+// //exe 4
+// const students = ['ana', 'givi', 'nona', 'data'];
+// students.forEach((student, index) => {
+//     console.log(`${student} aris ${index + 1}-e adgilze`);
+// });
+
+
+// //exe 5
+// const numbers = [5, 8, 10, 13, 16];
+// numbers.forEach(num => {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// });
+
+
+// //exe 7
+// const names = ['ANA', 'GOCHA', 'NINO'];
+// const lowerCaseNames = names.map(name => name.toLowerCase());
+// console.log(lowerCaseNames);
+
+
+
+// //exe 8 
+
+// const numbers = [5, 15, 25];
+// const gazrdili = numbers.map(num => num + 10);
+// console.log(gazrdili);
+
+
+
+// //exe 10
+
+// const numbers = [1, 6, 5, 12];
+// const kentiGaormagda = numbers.map(num => num % 2 === 0 ? num * 2 : num);
+// console.log(kentiGaormagda);
+
+
+// //exe 11
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const luwebi = numbers.filter(num => num % 2 === 0);
+// console.log(luwebi);
+
+
+// //exe 12
+// const names = ['tako', 'mariami', 'george'];
+// const longNames = names.filter(name => name.length > 5);
+// console.log(longNames);
+
+
+// //exe 13
+// const numbers = [-5, 10, -3, 8];
+// const dadebitebi = numbers.filter(num => num > 0);
+// console.log(dadebitebi);
+
+
+// //exe 15
+// const numbers = [30, 60, 45, 80];
+// const moreThanFifty = numbers.filter(num => num > 50);
+// console.log(moreThanFifty);
+
+
+// //exe 16
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// //exe 17
+// const numbers = [5, 10, 15];
+// let i = 0
+// let sum = 0;
+// while (i < numbers.length) {
+//     sum += numbers[i];
+//     i++;
+// }
+// console.log(sum);
+
+
+// //exe 18
+// let num = 1;
+// while (num <= 20) {
+//     console.log(num);
+//     num += 2;
+// }
+
+
+// //exe 19
+// let num = 20;
+// while (num >= 10) {
+//     console.log(num);
+//     num--;
+// }
+
+
+
+// //exe 20
+// const objects = [
+//     { name: "obieqti 1" },
+//     { name: "obieqti 2" }
+// ];
+// let i = 0;
+// while (i < objects.length) {
+//     console.log(objects[i].name);
+//     i++;
+// }
